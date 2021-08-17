888181998ef9a4855abe4ec5a63aeb3cf061b648
TomoeMami <trembleafterme@Outlook.com>
2021-08-17 15:15:16 +0800

    上传于 2021年08月17日 15:15

--- data.py
+++ data.py
@@ -8,22 +8,32 @@ from pyecharts.options.global_options import InitOpts, TitleOpts
 from pyecharts.charts import Line,Grid
 import time 
 
+def mkdir(path):
+    # 去除首位空格
+    path=path.strip()
+    # 去除尾部 \ 符号
+    path=path.rstrip("\\")
+    path=path.encode('utf-8')
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists=os.path.exists(path)
+
+    # 判断结果
+    if not isExists:
+        os.makedirs(path)
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        return False
+
+
 rawpathdict = {
-    'a' : {
-        '1': './虚拟主播区专楼/虚拟主播【Asoul】/',
-    },
-    'b' : {
-        '1': './虚拟主播区专楼/虚拟主播【B综】/',
-    },
-    'h' : {
-       '1' : './虚拟主播区专楼/虚拟主播【H综】/',
-    },
-    'm' : {
-        '1': './虚拟主播区专楼/虚拟主播【Mea楼】/',
-    },
-    'v' : {
-        '1': './虚拟主播区专楼/虚拟主播【V综】/',
-    },
+    'a' :  './虚拟主播区专楼/虚拟主播【Asoul】/',
+    'b' :  './虚拟主播区专楼/虚拟主播【B综】/',
+    'h' :  './虚拟主播区专楼/虚拟主播【H综】/',
+    'm' :  './虚拟主播区专楼/虚拟主播【Mea楼】/',
+    'v' :  './虚拟主播区专楼/虚拟主播【V综】/',
 }
 # apaths = ['./S1PlainTextGeneral/虚拟主播区专楼/虚拟主播【Asoul】/','./S1PlainTextBackup/虚拟主播区专楼/2019599[【A18】A-SOUL讨论楼（8月8日，20：00，乃琳生日会）]/']
 
@@ -37,15 +47,13 @@ rawpathdict = {
 
 # vpaths = ['./S1PlainTextGeneral/虚拟主播区专楼/虚拟主播【V综】/','./S1PlainTextBackup/虚拟主播区专楼/1972669[【V14】虚拟YouTuber(vtuber)综合讨论楼]/']
 
-def getallfile(dirpath,allpath=[]):
+def get2levelfile(dirpath,allpath):
     for pa in Path(dirpath).iterdir():
         if Path(pa).is_dir():
-            print(pa)
-            getallfile(pa)
+            for p in Path(pa).iterdir():
+                allpath.append(p)
         else:
             allpath.append(pa) 
-    return allpath
-
 # def getkwfile(flist, keyword):
 #     res = []
 #     for ff in flist:
@@ -57,88 +65,65 @@ if __name__ == "__main__":
     pathdict = {}
     for key in rawpathdict.keys():
         count = 0
-        for i in rawpathdict[key].keys():
-            pathdict[key] = {}
-            if i == 'A':
-                pathdict[key][str(count)] = rawpathdict[key][i]
-                count = count + 1
+        temppaths = []
+        pathdict[key] = {}
+        get2levelfile(rawpathdict[key],temppaths)
+        for k in temppaths :
+            pathdict[key][str(count)] = k
+            count = count + 1
+    for key in pathdict.keys():
+        rawdata = []
+        for filepath in pathdict[key].values():
+            with open (filepath, 'r',encoding='UTF-8') as f:
+                lines = f.readlines() 
+                a = ''
+                for line in lines:
+                    a += line.strip()
+                    # a += line
+
+            b = a.split("*****")
+            res = []
+            for post in b:
+                post1 = post
+                post2 = post
+                data={}
+                data['id'] = ''.join(re.findall(r"^[\*]{0,2}####\s\s([^#]+)#", post))
+                # data['level'] = str(filepath)+''.join(re.findall(r"#####\s(\d+)#", post1))
+                data['time'] = ''.join(re.findall(r"^.*?发表于\s(\d{4}-\d{1,2}-\d{1,2} \d{2}:\d{2})", post2))
+                if(data['id']):
+                    res.append(data)
+            rawdata = rawdata + res
+
+
+        data = {}
+        for post in rawdata:
+            postintime = int(time.mktime(time.strptime(post['time'],"%Y-%m-%d %H:%M")))
+            if postintime%86400 :
+                postintime = postintime - postintime%86400
+            posttime = time.strftime("%Y-%m-%d", time.localtime(postintime))
+            if posttime not in data.keys():
+                data[posttime] = {}
+                data[posttime]['num'] = 1
+                data[posttime]['ids'] = {}
             else:
-                temppaths =  getallfile(rawpathdict[key][i])
-                for k in temppaths :
-                    pathdict[key][str(count)] = k
-                    count = count + 1
-                    print(k)
-        # for filepath in apath:
-        #     with open (filepath, 'r',encoding='UTF-8') as f:
-        #         lines = f.readlines() 
-        #         a = ''
-        #         for line in lines:
-        #             a += line.strip()
-        #             # a += line
+                data[posttime]['num'] = data[posttime]['num'] +1
+                data[posttime]['ids'][post['id']] = 1
 
-        #     b = a.split("*****")
-
-    #         res = []
-    #         for post in b:
-    #             post1 = post
-    #             post2 = post
-    #             data={}
-    #             data['id'] = ''.join(re.findall(r"^[\*]{0,2}####\s\s([^#]+)#", post))
-    #             # data['level'] = str(filepath)+''.join(re.findall(r"#####\s(\d+)#", post1))
-    #             data['time'] = ''.join(re.findall(r"^.*?发表于\s(\d{4}-\d{1,2}-\d{1,2} \d{2}:\d{2})", post2))
-    #             if(data['id']):
-    #                 res.append(data)
-    #         spath = ''.join(re.findall(r"\d{7}-\d{2}", str(filepath)))
-    #         with open(rawdatapath+spath+'.json',"w",encoding='utf-8') as f:
-    #                     f.write(json.dumps(res,indent=2,ensure_ascii=False))
-    #         print(filepath)
-
-    # allpath = getallfile(rawdatapath)                              
-    # rawdata = []
-    # with open('/home/riko/S1AllStars/V区专楼发言数量变迁/V-RawData.json', "w", encoding="utf-8") as f0:
-    #     for filepath in allpath:
-    #         print(filepath)
-    #         with open(filepath, "r", encoding="utf-8") as f1:
-    #             thdata = json.load(f1)
-    #             rawdata = rawdata + thdata
-    #             f1.close()
-    #     f0.write(json.dumps(rawdata,indent=2,ensure_ascii=False))
-
-
-    # with open('/home/riko/S1AllStars/V区专楼发言数量变迁/M-RawData.json', "r", encoding="utf-8") as f:
-    #     rawdata = json.load(f)
-    # data = {}
-    # for post in rawdata:
-    #     postintime = int(time.mktime(time.strptime(post['time'],"%Y-%m-%d %H:%M")))
-    #     if postintime%86400 :
-    #         postintime = postintime - postintime%86400
-    #     posttime = time.strftime("%Y-%m-%d", time.localtime(postintime))
-    #     if posttime not in data.keys():
-    #         data[posttime] = {}
-    #         data[posttime]['num'] = 1
-    #         data[posttime]['ids'] = {}
-    #     else:
-    #         data[posttime]['num'] = data[posttime]['num'] +1
-    #         data[posttime]['ids'][post['id']] = 1
-    # with open('/home/riko/S1AllStars/V区专楼发言数量变迁/M-DataDict.json', "w", encoding="utf-8") as f:
-    #     f.write(json.dumps(data,indent=2,ensure_ascii=False))
-
-
-    # with open('/home/riko/S1AllStars/V区专楼发言数量变迁/H-DataDict.json', "r", encoding="utf-8") as f:
-    #     datadict = json.load(f)
-    # data = []
-    # stime = []
-    # reply = []
-    # replyer = []
-    # for key in datadict.keys():
-    #     c = key
-    #     d = datadict[key]['num']
-    #     e = len(datadict[key]['ids'].keys())
-    #     stime.append(c)
-    #     reply.append(d)
-    #     replyer.append(e)
-    # data.append(stime)
-    # data.append(reply)
-    # data.append(replyer)
-    # with open('/home/riko/S1AllStars/V区专楼发言数量变迁/H-Data.json', "w", encoding="utf-8") as f:
-    #     f.write(json.dumps(data,indent=2,ensure_ascii=False))
+        datadict = data
+        data = []
+        stime = []
+        reply = []
+        replyer = []
+        for keyd in datadict.keys():
+            c = keyd
+            d = datadict[keyd]['num']
+            e = len(datadict[keyd]['ids'].keys())
+            stime.append(c)
+            reply.append(d)
+            replyer.append(e)
+        data.append(stime)
+        data.append(reply)
+        data.append(replyer)
+        mkdir('./S1G/')
+        with open('./S1G/'+key+'-Data.json', "w", encoding="utf-8") as f:
+            f.write(json.dumps(data,indent=2,ensure_ascii=False))
